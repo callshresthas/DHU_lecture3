f50487d770a15e33d16e52e2707c84323f04df47
callmebreaks <shrestha@shrestha-no-MacBook-Pro.local>
2013-11-06 14:20:07 +0900

    express
    vv

--- index.php1.php
+++ index.php1.php
@@ -1,2 +1,11 @@
 <?php
 
+today = time();
+
+date_default_timezone_set('Asia/Tokyo');
+echo date('c', $today);
+echo '<br />';
+
+date_default_timezone_set('Europe/Paris');
+echo date('c', $today);
+echo '<b
